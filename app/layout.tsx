481e07eb1f883eb3f0ac44d5d7ebc489bd178693
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-11-02 23:06:04 -0300

    Organized code from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google"
-import "./globals.css"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kimetsu no Yaiba",
   icons: {
     icon: "/dev-demon-slayer/favicon-demon-slayer.png",
   },
-  description: "Tudo sobre o universo de Demon Slayer: personagens, episódios e temporadas.",
-    generator: 'v0.dev'
-}
+  description:
+    "Tudo sobre o universo de Demon Slayer: personagens, episódios e temporadas.",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
@@ -29,10 +30,12 @@ export default function RootLayout({
       <body className={`${inter.className} bg-black text-white`}>
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
+          <main className="flex-grow container mx-auto px-4 py-8">
+            {children}
+          </main>
           <Footer />
         </div>
       </body>
     </html>
-  )
+  );
 }
