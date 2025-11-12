533d92da13f718419a8fa5e3aa7b9fc5dc1d9d81
OticasVisiotech <gabrielmgb.nd@gmail.com>
2025-11-11 23:36:11 -0300

    Organized code from lib type

--- lib/type.ts
+++ lib/type.ts
@@ -1,10 +1,10 @@
 export type Character = {
-  id: number
-  name: string
-  image: string
-  banner: string
-  bio: string
-  affiliation: string
-  breathing: string
-  trivia: string
-}
+  id: number;
+  name: string;
+  image: string;
+  banner: string;
+  bio: string;
+  affiliation: string;
+  breathing: string;
+  trivia: string;
+};
